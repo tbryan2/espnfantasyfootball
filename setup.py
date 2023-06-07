d1062a9c1d7f8f811775bb326b361d5c3527c26a
tbryan2 <timbryan0315@gmail.com>
2023-06-06 19:15:57 -0500

    cleanup and package preparation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='espnfantasyfootball',
+    version='0.1.0',
+    description='A Python package to interact with ESPN Fantasy Football data',
+    url='https://github.com/tbryan2/espnfantasyfootball',
+    author='Tim Bryan',
+    license='MIT',
+    packages=['espnfantasyfootball'], 
+    install_requires=['requests', 'pandas']
+)
